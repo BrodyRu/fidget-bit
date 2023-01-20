8ec7d7fcc78e39a63a8d4c81c1366e832fa615c5
BrodyRu <122548122+BrodyRu@users.noreply.github.com>
2023-01-20 10:16:22 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 let rps = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
 input.onGesture(Gesture.Shake, function () {
     rps = randint(1, 3)
     if (rps == 1) {
